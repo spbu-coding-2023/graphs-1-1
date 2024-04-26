9b5855f2265eeb4e0a190e3665efc3faba86b18f
Lkk9 <l.t.446kk@gmail.com>
2024-04-26 22:29:54 +0300

    init with kmp wizard

---
+++ composeApp/src/desktopMain/kotlin/App.kt
@@ -0,0 +1,25 @@
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+@Composable
+@Preview
+fun App() {
+    MaterialTheme {
+        var counter by remember { mutableStateOf(0) }
+
+        Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
+            Button(onClick = { counter++ }) {
+                Text("Inc")
+            }
+
+            Text("counter: $counter")
+        }
+    }
+}
