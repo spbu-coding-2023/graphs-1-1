071d73dbda67fbcb6c9dd03ebeff7d4e3ceae103
kinokotakenoko9 <chdmitry2@gmail.com>
2024-05-09 13:01:26 +0300

    add: graph interface

---
+++ library/src/main/kotlin/graph/Graph.kt
@@ -0,0 +1,84 @@
+package graph
+
+interface Graph<V, E> {
+
+    /**
+     * Adds vertex to the graph if not present
+     */
+    fun addVertex(v: V)
+    // fun addVertex(): V
+
+    /**
+     * Adds Edge between tail and head to the graph if not present
+     */
+    fun addEdge(tail: V, head: V, e: E)
+    // fun addEdge(tail: V, head: V): E
+
+    /**
+     * Returns true if vertex is in the graph
+     */
+    fun containsVertex(v: V): Boolean
+
+    /**
+     * Returns true if edge between tail and head is in the graph
+     */
+    fun containsEdge(tail: V, head: V): Boolean
+    // fun containsEdge(e: E): Boolean
+
+    /**
+     * Returns the degree of a vertex
+     */
+    fun degreeOf(v: V): Int
+
+    /**
+     * Returns set of all edges in graph
+     */
+    fun edgeSet(): Set<E>
+
+    /**
+     * Returns set of all edges connected to a vertex in graph (any direction)
+     */
+    fun edgesOf(v: V): Set<E>
+
+    /**
+     * Returns edge between vertex tail and vertex head
+     */
+    fun getEdge(tail: V, head: V): E
+
+    /**
+     * Returns weight of an edge
+     */
+    fun getEdgeWeight(e: E): Double
+
+    /**
+     * Returns all incoming edges in vertex
+     */
+    fun incomingEdgesOf(v: V): Set<E>
+
+    /**
+     * Returns all outgoing edges in vertex
+     */
+    fun outgoingEdgesOf(v: V): Set<E>
+
+    /**
+     * Removes edge and returns true if edge was present in the graph
+     */
+    fun removeEdge(e: E): Boolean
+    // fun removeEdge(tail: V, head: V): Boolean
+
+    /**
+     * Removes vertex and all its touching edges
+     */
+    fun removeVertex(v: V): Boolean
+
+    /**
+     * Sets weight to an edge
+     */
+    fun setEdgeWeight(e: E, w: Double)
+    // fun setEdgeWeight(tail: V, head: V, w: Double)
+
+    /**
+     * Returns set of all vertecies in the graph
+     */
+    fun vertexSet(): Set<V>
+}
