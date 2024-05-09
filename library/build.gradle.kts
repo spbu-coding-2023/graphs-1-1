63c200a67d003d80a9af8025d43b17d3ef1789e1
kinokotakenoko9 <chdmitry2@gmail.com>
2024-05-09 22:32:50 +0300

    add: include dependencies, add simple ui
    also update detekt config, load pic, and add keep file

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -7,8 +7,34 @@ dependencies {
     // use JUnit5
     testImplementation(libs.junit.api)
     testImplementation(libs.junit.engine)
+
+    // gephi toolkit
+    implementation(files("libs/gephi-toolkit-0.10.0-all.jar"))
 }
 
 tasks.named<Test>("test") {
     useJUnitPlatform()
+}
+
+tasks.register("downloadGephiToolkit") {
+    val path = "libs/gephi-toolkit-0.10.0-all.jar"
+    val sourceUrl = "https://github.com/gephi/gephi-toolkit/releases/download/v0.10.0/gephi-toolkit-0.10.0-all.jar"
+
+    val libsDirectory = File("library/libs")
+    val jarFile = File(path)
+
+    if (!libsDirectory.exists())
+        libsDirectory.mkdir()
+
+    if (!jarFile.exists())
+        download(sourceUrl, path)
+}
+
+tasks.build {
+    dependsOn("downloadGephiToolkit")
+}
+
+fun download(url: String, path: String){
+    val destinationFile = File(path)
+    ant.invokeMethod("get", mapOf("src" to url, "dest" to destinationFile))
 }
