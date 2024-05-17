f5f8c04558342d1605ee726c350d5be416426ede
kinokotakenoko9 <chdmitry2@gmail.com>
2024-05-17 18:30:22 +0300

    add: voyager lib

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -1,4 +1,5 @@
 import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+import org.jetbrains.kotlin.psi.simpleNameExpressionVisitor
 
 plugins {
     alias(libs.plugins.kotlin.jvm)
@@ -11,7 +12,6 @@ dependencies {
     implementation(compose.foundation)
     implementation(compose.material3)
     implementation(compose.ui)
-    implementation(compose.components.resources)
     implementation(compose.components.uiToolingPreview)
     // // add library directory
     implementation(projects.library)
@@ -21,6 +21,17 @@ dependencies {
 
     // resources
     implementation(compose.components.resources)
+
+    // serialization
+//    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
+
+    // Voyager
+    val voyagerVersion = "1.0.0"
+    implementation("cafe.adriel.voyager:voyager-navigator:$voyagerVersion")
+    implementation("cafe.adriel.voyager:voyager-screenmodel:$voyagerVersion")
+    implementation("cafe.adriel.voyager:voyager-bottom-sheet-navigator:$voyagerVersion")
+    implementation("cafe.adriel.voyager:voyager-tab-navigator:$voyagerVersion")
+    implementation("cafe.adriel.voyager:voyager-transitions:$voyagerVersion")
 }
 
 compose.desktop {
