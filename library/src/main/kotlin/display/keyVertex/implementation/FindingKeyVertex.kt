b977368c1f4140cc6a8cd0afa1cf21e25c9145d2
mshipilov5 <shipilovmaksim05@gmail.com>
2024-05-21 04:10:55 +0300

    fix: dependencies between functions. changed variable names

--- library/src/main/kotlin/display/keyVertex/implementation/FindingKeyVertex.kt
+++ library/src/main/kotlin/display/keyVertex/implementation/FindingKeyVertex.kt
@@ -7,8 +7,8 @@ import java.util.*
 
 
 fun successors(v: V): Set<V> {
-    val incoming = incomingVerteciesOf(v)
-    val outgoing = outgoingVerteciesOf(v)
+    val incoming = graph.incomingVerteciesOf(v)
+    val outgoing = graph.outgoingVerteciesOf(v)
     return incoming.union(outgoing)
 }
 
@@ -17,41 +17,41 @@ class GraphBetweennessCentrality : GraphKeyVertex {
     override fun <V, E> getKeyVertecies(graph: Graph<V, E>): Map<V, Float> {
         val betweennessMap = mutableMapOf<V, Float>().withDefault { 0f }
         val vertices = graph.vertexSet()
-        for (s in vertices) {
+        for (sourceVertex in vertices) {
             val stack = Stack<V>()
             val predecessors = mutableMapOf<V, MutableList<V>>().withDefault { mutableListOf() }
-            val sigma = mutableMapOf<V, Float>().withDefault { 0f }
+            val sourceVertexWeight = mutableMapOf<V, Float>().withDefault { 0f }
             val distance = mutableMapOf<V, Int>().withDefault { -1 }
-            val delta = mutableMapOf<V, Float>().withDefault { 0f }
+            val sourceVertexDependency = mutableMapOf<V, Float>().withDefault { 0f }
 
-            sigma[s] = 1f
-            distance[s] = 0
+            sourceVertexWeight[sourceVertex] = 1f
+            distance[sourceVertex] = 0
 
             val queue: Queue<V> = LinkedList<V>()
-            queue.add(s)
+            queue.add(sourceVertex)
 
             while (queue.isNotEmpty()) {
-                val v = queue.remove()
-                stack.push(v)
-                for (w in graph.successors(v)) {
-                    if (distance[w] == -1) {
-                        queue.add(w)
-                        distance[w] = distance[v]!! + 1
+                val currentVertex = queue.remove()
+                stack.push(currentVertex)
+                for (successorVertex in successors(currentVertex)) {
+                    if (distance[successorVertex] == -1) {
+                        queue.add(successorVertex)
+                        distance[successorVertex] = distance[currentVertex]!! + 1
                     }
-                    if (distance[w] == distance[v]!! + 1) {
-                        sigma[w] = sigma[w]!! + sigma[v]!!
-                        predecessors[w]!!.add(v)
+                    if (distance[successorVertex] == distance[currentVertex]!! + 1) {
+                        sourceVertexWeight[successorVertex] = sourceVertexWeight[successorVertex]!! + sourceVertexWeight[currentVertex]!!
+                        predecessors[successorVertex]!!.add(currentVertex)
                     }
                 }
             }
 
             while (stack.isNotEmpty()) {
-                val w = stack.pop()
-                for (v in predecessors[w]!!) {
-                    delta[v] = delta[v]!! + (sigma[v]!! / sigma[w]!!) * (1 + delta[w]!!)
+                val successorVertex = stack.pop()
+                for (currentVertex in predecessors[successorVertex]!!) {
+                    sourceVertexDependency[currentVertex] = sourceVertexDependency[currentVertex]!! + (sourceVertexWeight[currentVertex]!! / sourceVertexWeight[successorVertex]!!) * (1 + sourceVertexDependency[successorVertex]!!)
                 }
-                if (w != s) {
-                    betweennessMap[w] = betweennessMap[w]!! + delta[w]!!
+                if (successorVertex != sourceVertex) {
+                    betweennessMap[successorVertex] = betweennessMap[successorVertex]!! + sourceVertexDependency[successorVertex]!!
                 }
             }
         }
