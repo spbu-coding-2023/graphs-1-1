4af2690f4f5ce23595a2a01f8fcde852db888c27
Roman Mukovenkov <roman.mukovenkov@gmail.com>
2024-05-24 23:11:40 +0300

    add: more tests to strongly connected component search, also fix it so that there isn't anything done when called for an undirected graph

--- library/src/main/kotlin/display/stronglyConnectedComponentSearch/implementation/GraphSCCSearch.kt
+++ library/src/main/kotlin/display/stronglyConnectedComponentSearch/implementation/GraphSCCSearch.kt
@@ -6,40 +6,52 @@ import java.util.Stack
 import kotlin.collections.HashMap
 import kotlin.math.min
 
-
 class GraphSCCSearchWithTarjan<V, E> : GraphSCCSearch<V, E> {
-    private var index = 0
-    private val indexMap = HashMap<V, Int>()
-    private val lowLinkMap = HashMap<V, Int>()
-    private val stack = Stack<V>()
-    private val onStack = HashSet<V>()
-    private val SCCs = mutableSetOf<Set<V>>()
-    override fun getSCCs(graph: Graph<V, E>) : Set<Set<V>> {
-        for (v : V in graph.vertexSet()) {
+    override fun getSCCs(graph: Graph<V, E>) : Set<Set<V>>? {
+        if (graph.configuration.isUndirected()) {
+            return null
+        }
+
+        val indexMap = HashMap<V, Int>()
+        val lowLinkMap = HashMap<V, Int>()
+        val stack = Stack<V>()
+        val onStack = HashSet<V>()
+        val SCCs = mutableSetOf<Set<V>>()
+
+        var index = 0
+        for (v: V in graph.vertexSet()) {
             if (indexMap[v] == null) {
-                strongConnect(graph, v)
+                index = strongConnect(graph, v, index, indexMap, lowLinkMap, stack, onStack, SCCs)
             }
         }
         return SCCs
     }
 
-    private fun strongConnect(graph: Graph<V, E>, v : V) {
+    private fun strongConnect(
+        graph: Graph<V, E>, v: V, currentIndex: Int,
+        indexMap: HashMap<V, Int>, lowLinkMap: HashMap<V, Int>,
+        stack: Stack<V>, onStack: HashSet<V>,
+        SCCs: MutableSet<Set<V>>
+    ): Int {
+        var index = currentIndex
         indexMap[v] = index
         lowLinkMap[v] = index
         index++
         stack.push(v)
         onStack.add(v)
+
         for (outgoingNeighbour in graph.outgoingVerticesOf(v)) {
             if (indexMap[outgoingNeighbour] == null) {
-                strongConnect(graph, outgoingNeighbour)
+                index = strongConnect(graph, outgoingNeighbour, index, indexMap, lowLinkMap, stack, onStack, SCCs)
                 lowLinkMap[v] = min(lowLinkMap[v]!!, lowLinkMap[outgoingNeighbour]!!)
             } else if (onStack.contains(outgoingNeighbour)) {
                 lowLinkMap[v] = min(lowLinkMap[v]!!, indexMap[outgoingNeighbour]!!)
             }
         }
+
         if (lowLinkMap[v] == indexMap[v]) {
             val component = mutableSetOf<V>()
-            var w : V
+            var w: V
             do {
                 w = stack.pop()
                 onStack.remove(w)
@@ -47,5 +59,7 @@ class GraphSCCSearchWithTarjan<V, E> : GraphSCCSearch<V, E> {
             } while (w != v)
             SCCs.add(component)
         }
+
+        return index
     }
 }
