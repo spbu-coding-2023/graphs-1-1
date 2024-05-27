133e578642e5d18e7b32a051e3af72a33a953884
Roman Mukovenkov <roman.mukovenkov@gmail.com>
2024-05-27 06:22:00 +0300

    add: my own database graph exporter and importer, since the normal one isn't compatible at all

--- library/src/main/kotlin/repository/DatabaseGraphImporter.kt
+++ library/src/main/kotlin/repository/DatabaseGraphImporter.kt
@@ -5,7 +5,7 @@ import java.io.File
 
 interface DatabaseGraphImporter {
     /**
-     * Imports a graph with a certain id within a database
+     * Imports a graph with a certain id with its metadata within a database
      */
-    fun <V, E>importGraph(graphId: String, credentials: List<String>): Graph<V, E>
+    fun <V, E>importGraph(graphId: String, credentials: List<String?>): Graph<V, E>
 }
