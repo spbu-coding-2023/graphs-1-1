5bb9b0471f5dd1515cabd5508fdc989473437294
Roman Mukovenkov <roman.mukovenkov@gmail.com>
2024-05-27 06:22:00 +0300

    add: my own database graph exporter and importer, since the normal one isn't compatible at all

--- library/src/main/kotlin/repository/DatabaseGraphExporter.kt
+++ library/src/main/kotlin/repository/DatabaseGraphExporter.kt
@@ -5,7 +5,8 @@ import java.io.File
 
 interface DatabaseGraphExporter {
     /**
-     * Exports graph to a file, needs credentials. Outputs a unique string that is used to access this graph in the database
+     * Exports graph to a file, needs credentials.
+     * Outputs a unique string graphId that is used to access everything (all nodes and edges have this same graphId)
      */
-    fun <V, E>exportGraph(graph: Graph<V, E>, credentials: List<String>): String
+    fun <V, E>exportGraph(graph: Graph<V, E>, credentials: List<String?>): String
 }
