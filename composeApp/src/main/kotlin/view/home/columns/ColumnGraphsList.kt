86b9f3a81c180c9c7dfd373d6bde27f0327353cf
Roman Mukovenkov <roman.mukovenkov@gmail.com>
2024-05-30 00:04:27 +0300

    add: import button after every graph representation

--- composeApp/src/main/kotlin/view/home/columns/ColumnGraphsList.kt
+++ composeApp/src/main/kotlin/view/home/columns/ColumnGraphsList.kt
@@ -1,6 +1,7 @@
 package view.home.columns
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.rememberScrollState
@@ -14,7 +15,10 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.dp
 import viewModel.workspace.graph.GraphsContainerViewModel
 
@@ -74,7 +78,66 @@ fun ColumnGraphsList(modifier: Modifier, graphsContainerViewModel: GraphsContain
                             titleColor = MaterialTheme.colorScheme.primary,
                             subTitleColor = MaterialTheme.colorScheme.secondary
                         )
+
                     }
+                }
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp, 4.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Row(
+                        modifier = Modifier
+                            .clip(shape = MaterialTheme.shapes.small)
+                            .weight(1f)
+                            .clickable {
+
+                            },
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Spacer(modifier = Modifier.width(8.dp))
+
+                        Surface(
+                            modifier = Modifier
+                                .size(40.dp),
+                            shape = MaterialTheme.shapes.medium,
+                        ) {
+                            Column(
+                                horizontalAlignment = Alignment.CenterHorizontally,
+                                verticalArrangement = Arrangement.Center,
+                                modifier = Modifier.fillMaxSize()
+                                    .background(brush = Brush.linearGradient(
+                                        colors = listOf(Color.Gray, Color.DarkGray),
+                                        start = Offset(0f, 0f),
+                                        end = Offset(50f*density, 50f*density)
+                                    ))
+                            ) {
+                                Text(
+                                    text = "+",
+                                    fontSize = MaterialTheme.typography.bodyMedium.fontSize,
+                                    color = Color.White
+                                )
+                            }
+                        }
+
+                        Spacer(modifier = Modifier.width(8.dp))
+
+                        Column(
+                            modifier = Modifier
+                                .padding(8.dp),
+                            verticalArrangement = Arrangement.spacedBy(0.dp, Alignment.Bottom),
+
+                            ) {
+                            Text(
+                                text = "Import your own graph here!",
+                                color = MaterialTheme.colorScheme.primary,
+                                fontSize = MaterialTheme.typography.bodyLarge.fontSize,
+                            )
+
+                        }
+                    }
+                }
                 }
             }
         }
