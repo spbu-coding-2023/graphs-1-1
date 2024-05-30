e1fdb169b33a3ca8c0c0f531a1fb57c8b67949b2
mshipilov5 <shipilovmaksim05@gmail.com>
2024-05-30 19:28:39 +0300

    fix: Dijkstra correct work on random generated graphs

--- library/src/main/kotlin/display/pathSearch/implementation/GraphPathSearchDijkstra.kt
+++ library/src/main/kotlin/display/pathSearch/implementation/GraphPathSearchDijkstra.kt
@@ -2,50 +2,57 @@ package display.pathSearch.implementation
 
 import display.pathSearch.GraphPathSearch
 import graph.Graph
+import java.util.PriorityQueue
 
 class GraphDijkstraPathFinder : GraphPathSearch {
 
     override fun <V, E> searchPath(graph: Graph<V, E>, startingVertex: V, endingVertex: V): List<V> {
         val distance = mutableMapOf<V, Double>()
         val predecessor = mutableMapOf<V, V?>()
-        val unvisitedVertices = mutableSetOf<V>()
+        val vertexEdges = graph.edgeSetOfVertices()
+        val path = mutableListOf<V>()
+
+        val priorityQueue = PriorityQueue<Pair<V, Double>>(compareBy { it.second })
 
         for (vertex in graph.vertexSet()) {
             distance[vertex] = Double.POSITIVE_INFINITY
             predecessor[vertex] = null
-            unvisitedVertices.add(vertex)
         }
 
         distance[startingVertex] = 0.0
+        priorityQueue.add(Pair(startingVertex, 0.0))
 
-        while (unvisitedVertices.isNotEmpty()) {
-            val currentVertex = unvisitedVertices.minByOrNull { distance[it]!! } ?: break
-            unvisitedVertices.remove(currentVertex)
+        while (priorityQueue.isNotEmpty()) {
+            val (currentVertex, currentDistance) = priorityQueue.poll()
 
-            if (currentVertex == endingVertex) break
+            if (currentDistance > distance[currentVertex]!!) continue
 
-            for (edge in graph.outgoingEdgesOf(currentVertex)) {
-                val neighbor = graph.getEdgeHead(edge)
-                val edgeWeight = graph.getEdgeWeight(edge)
-                val newDistance = distance[currentVertex]!! + edgeWeight
+            for ((edgeTail, edgeHead) in vertexEdges) {
+                if (edgeTail == currentVertex) {
+                    val neighbor = edgeHead
+                    val edgeWeight = graph.getEdgeWeight(edgeTail, neighbor)
+                    val newDistance = distance[currentVertex]!! + edgeWeight
 
-                if (newDistance < distance[neighbor]!!) {
-                    distance[neighbor] = newDistance
-                    predecessor[neighbor] = currentVertex
+                    if (newDistance < distance[neighbor]!!) {
+                        distance[neighbor] = newDistance
+                        predecessor[neighbor] = currentVertex
+                        priorityQueue.add(Pair(neighbor, newDistance))
+                    }
                 }
             }
         }
 
-        // Проверка на случай, когда путь не найден
         if (predecessor[endingVertex] == null && startingVertex != endingVertex) {
-            return mutableListOf()
+            return listOf()
         }
 
-        val path = mutableListOf<V>()
         var currentVertex = endingVertex
         while (currentVertex != startingVertex) {
+            if (predecessor[currentVertex] == null) {
+                return listOf()
+            }
             path.add(currentVertex)
-            currentVertex = predecessor[currentVertex] ?: return mutableListOf() // Путь не найден
+            currentVertex = predecessor[currentVertex]!!
         }
         path.add(startingVertex)
 
